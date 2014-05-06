177aca6b9415d617b77fae262a32d7b4ac399ef6
John Clem <johnnyclem@gmail.com>
2014-05-06 10:16:58 -0700

    add text fields and signup button

--- SignUp Form/ViewController.m
+++ SignUp Form/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordConfirmTextField;
+@property (weak, nonatomic) IBOutlet UIButton *signUpButton;
 
 @end
 
@@ -24,6 +28,16 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// listen for keyboard event
+// make sure it's a valid email address
+// password and password confirmation should match
+// password strength checker / valid password
+
+- (IBAction)signUp:(id)sender
+{
+    
 }
 
 @end
